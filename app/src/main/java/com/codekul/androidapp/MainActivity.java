cfbf9e2e061fbc1455a0fdd99ea2a3f20518e3f5
aniruddha <aniruddha.kudalkar@gmail.com>
2016-11-16 18:13:13 +0530

    creation of ui using code, and click listener

--- app/src/main/java/com/codekul/androidapp/MainActivity.java
+++ app/src/main/java/com/codekul/androidapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.codekul.androidapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        MainActivity mainActivity = this;
+        Context context = mainActivity;
+        context = this;
+
+        LinearLayout.LayoutParams paramRoot
+                = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+        rootLayout.setLayoutParams(paramRoot);
+        setContentView(rootLayout);
+
+        LinearLayout.LayoutParams paramsBtn =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        Button btnOkay = new Button(this);
+        btnOkay.setLayoutParams(paramsBtn);
+        btnOkay.setText("Okay");
+        btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        rootLayout.addView(btnOkay);
+
+        LinearLayout.LayoutParams paramsText =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        TextView text = new TextView(this);
+        text.setLayoutParams(paramsText);
+        text.setText("I am TextView");
+        rootLayout.addView(text);
+    }
+
+    private class Click implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
