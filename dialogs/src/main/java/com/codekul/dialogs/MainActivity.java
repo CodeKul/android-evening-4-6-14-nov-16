ecc00b0c417d9408f2e4fd3be8a1f25226acf0c4
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-06 18:43:56 +0530

    dialogs , alert , date picker, time picker, progress, custom dialog

---
+++ dialogs/src/main/java/com/codekul/dialogs/MainActivity.java
@@ -0,0 +1,43 @@
+package com.codekul.dialogs;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnAlert).setOnClickListener(this::clicked);
+        findViewById(R.id.btnDatePicker).setOnClickListener(this::clicked);
+        findViewById(R.id.btnTimePicker).setOnClickListener(this::clicked);
+        findViewById(R.id.btnProgress).setOnClickListener(this::clicked);
+        findViewById(R.id.btnCustom).setOnClickListener(this::clicked);
+    }
+
+    private void clicked(View view) {
+        if(view.getId() == R.id.btnAlert) showDialog(MyDialog.DIALOG_ALERT);
+        if(view.getId() == R.id.btnDatePicker) showDialog(MyDialog.DIALOG_DATE_PICKER);
+        if(view.getId() == R.id.btnTimePicker)showDialog(MyDialog.DIALOG_TIME_PICKER);
+        if(view.getId() == R.id.btnProgress) showDialog(MyDialog.DIALOG_PROGRESS);
+        if(view.getId() == R.id.btnCustom) showDialog(MyDialog.DIALOG_CUSTOM);
+    }
+
+    private void showAlert() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.mipmap.ic_launcher)
+                .setMessage(R.string.message)
+                .setTitle(R.string.title);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private void showDialog(String tag) {
+        MyDialog dialog = new MyDialog();
+        dialog.show(getSupportFragmentManager(),tag);
+    }
+}
---
+++ dialogs/src/main/java/com/codekul/dialogs/MyDialog.java
@@ -0,0 +1,106 @@
+package com.codekul.dialogs;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyDialog extends DialogFragment {
+
+    public static final String DIALOG_ALERT = "alert";
+    public static final String DIALOG_DATE_PICKER = "datePicker";
+    public static final String DIALOG_TIME_PICKER = "timePicker";
+    public static final String DIALOG_PROGRESS = "progress" ;
+    public static final String DIALOG_CUSTOM = "custom";
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Dialog dialog  = null;
+
+        if(getTag().equals(DIALOG_ALERT)) dialog = showAlertDialog();
+        if(getTag().equals(DIALOG_DATE_PICKER)) dialog = showDatePicker();
+        if(getTag().equals(DIALOG_TIME_PICKER)) dialog = showTimePicker();
+        if(getTag().equals(DIALOG_PROGRESS)) dialog = showProgressDialog();
+        if(getTag().equals(DIALOG_CUSTOM)) dialog = showCustomDialog();
+        return dialog;
+    }
+
+    private Dialog showCustomDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.custom_dialog,null,false);
+        view.findViewById(R.id.btnLogin).setOnClickListener(v -> mt("Login Clicked"));
+        builder.setView(view);
+        return builder.create();
+    }
+
+    private Dialog showProgressDialog() {
+
+        ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle(R.string.title);
+        progressDialog.setMessage(getResources().getString(R.string.message));
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setButton(DialogInterface.BUTTON_POSITIVE,getResources().getString(R.string.btnYes),(dialog, which) -> mt("Yes Clicked"));
+        progressDialog.setMax(100);
+        progressDialog.setProgress(45);
+        return progressDialog;
+    }
+
+    private Dialog showTimePicker() {
+
+        TimePickerDialog timePicker =
+                new TimePickerDialog(getActivity(),(view, hourOfDay, minute) -> mt(""+hourOfDay +" : "+ minute),6,8,true);
+
+        return timePicker;
+    }
+
+    private Dialog showDatePicker() {
+
+        //Calendar
+        DatePickerDialog datePicker = new DatePickerDialog(getActivity(), (view, year, month, dayOfMonth) -> mt(""+dayOfMonth +" - "+(month+1)+" - "+year) , 2017,0,1);
+        datePicker.getDatePicker().setMinDate(System.currentTimeMillis());
+        return  datePicker;
+    }
+
+    private AlertDialog showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.mipmap.ic_launcher)
+                .setMessage(R.string.message)
+                .setTitle(R.string.title)
+                .setPositiveButton(R.string.btnYes, this::alertButtonClicked)
+                .setNegativeButton(R.string.btnNo, this::alertButtonClicked);
+       return  builder.create();
+    }
+
+    private void alertButtonClicked(DialogInterface di, int which) {
+        if(which == DialogInterface.BUTTON_POSITIVE){
+            mt("Positive");
+        }
+        else if(which == DialogInterface.BUTTON_NEGATIVE) {
+            mt("Negative");
+        }
+        else {
+            mt("Neutral");
+        }
+    }
+    
+    private void mt(String msg){
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
+}
