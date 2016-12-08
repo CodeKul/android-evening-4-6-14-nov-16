350c2203d1421bdb60248e8e7881a34ca22125d0
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-08 17:02:52 +0530

    opening pdf using file provider

--- intentsandintentfilters/src/main/java/com/codekul/intentsandintentfilters/MainActivity.java
+++ intentsandintentfilters/src/main/java/com/codekul/intentsandintentfilters/MainActivity.java
@@ -1,10 +1,24 @@
 package com.codekul.intentsandintentfilters;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void click(View view) {
-        shareIntent();
+        openPdf();
     }
 
     private void openComman(){
@@ -73,5 +87,51 @@ public class MainActivity extends AppCompatActivity {
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
         startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+
+    private void openPdf() {
+        Uri path = FileProvider.getUriForFile(this, getApplicationContext()
+                    .getPackageName() + ".provider", copyPdfs("my.pdf"));
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(path,"application/pdf");
+        startActivity(intent);
+    }
+    private File copyPdfs(String fileName)
+    {
+        AssetManager assetManager = getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+        File  file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+        try
+        {
+            in = assetManager.open(fileName);
+            out = new FileOutputStream(file);
+
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return file;
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, read);
+        }
     }
 }
