53aab39d0ae71d8016e802f60a4e7ba9876b54df
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-27 17:40:54 +0530

    search view tool bar

---
+++ searchview/src/main/java/com/codekul/searchview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.codekul.searchview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SearchView mSearchView;
+    private MenuItem searchMenuItem;
+    private SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            Log.i("@codekul",""+newText);
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        searchMenuItem = menu.findItem(R.id.action_search);
+        mSearchView = (SearchView) searchMenuItem.getActionView();
+        mSearchView.setOnQueryTextListener(listener);
+        return true;
+    }
+}
